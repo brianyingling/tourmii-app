f359dc16599726437875c67b440d8e528f02eebd
Brian Yingling <brian_yingling@yahoo.com>
2014-05-19 13:35:47 -0400

    working on getting jasmine to find the module

--- test/controllers/loginCtrlSpec.js
+++ test/controllers/loginCtrlSpec.js
@@ -1,15 +1,31 @@
 describe('LoginCtrl', function() {
-  var $scope, $rootScope, $controller, createController;
+  var $scope, $rootScope, createController;
+  var loginCtrl;
+  // beforeEach(inject(function($injector) {
+  //   $rootScope  = $injector.get('$rootScope');
+  //   $scope      = $rootScope.$new();
+  //   $controller = $injector.get('$controller');
 
-  beforeEach(inject(function($injector) {
-    $rootScope  = $injector.get('$rootScope');
-    $scope      = $rootScope.$new();
-    $controller = $injector.get('$controller');
+  //   createController = function() {
+  //     return $controller('LoginCtrl', {'$scope':$scope});
+  //   };
+  // }));
 
-    createController = function() {
-      return $controller('LoginCtrl', {'$scope':$scope});
-    };
+  beforeEach(module('tourmii.controllers'));
+  beforeEach(inject(function($controller){
+    loginCtrl = $controller("LoginCtrl");
 
   }));
+
+  it('has a LoginCtrl controller', function() {
+      var scope = $rootScope.$new();
+      var ctrl  = $controller('LoginCtrl', {$scope: scope});
+      expect(createController()).toBeDefined();
+
+  });
+
+  // it('submits a email as a username', function() {
+  //   $scope.user.email === "bob";
+  // });
 
 });
