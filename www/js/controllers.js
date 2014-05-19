634bc80ce2239c8a82c336472cdad204b94831f8
Brian Yingling <brian_yingling@yahoo.com>
2014-05-19 17:14:11 -0400

    submit() makes a call to the server and fetches user data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,25 @@
 angular.module('tourmii.controllers', [])
 
-.controller('LoginCtrl', ['$scope', function($scope) {
-  debugger;
-  return true;
-  // $scope.user = {};
-  // $scope.user.email = "bob";
+.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.user = {};
+
+  $scope.submit = function(){
+    $scope.user.email    = $scope.email;
+    $scope.user.password = $scope.password;
+    delete $http.defaults.headers.common["X-Requested-With"];
+    $http.post("http://localhost:3000/login", $scope.user)
+      .success(function(data) {
+        // data is the user object with all of his/her tours
+        $scope.tours = data.user.tours;
+        console.log(data);
+      })
+      .failure(function(data) {
+        debugger;
+        console.log(data);
+      });
+  };
+
+
 }])
 
 .controller('RegisterCtrl', ['$scope', function($scope) {
