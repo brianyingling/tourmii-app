2376a4bb2b5ee749a406e20abd2e45e8ad5525d7
Brian Yingling <brian_yingling@yahoo.com>
2014-05-19 17:11:38 -0400

    loginCtrlSpec cleaned up a bit

--- test/controllers/loginCtrlSpec.js
+++ test/controllers/loginCtrlSpec.js
@@ -1,34 +1,25 @@
 describe('LoginCtrl', function() {
   var $scope, $rootScope, $controller, createController;
   var loginCtrl;
-  // beforeEach(inject(function($injector) {
-  //   $rootScope  = $injector.get('$rootScope');
-  //   $scope      = $rootScope.$new();
-  //   $controller = $injector.get('$controller');
-
-  //   createController = function() {
-  //     return $controller('LoginCtrl', {'$scope':$scope});
-  //   };
-  // }));
 
   beforeEach(module('tourmii.controllers'));
   beforeEach(inject(function($injector){
-    $rootScope = $injector.get('$rootScope');
-    $scope = $rootScope.$new();
+    $rootScope  = $injector.get('$rootScope');
+    $scope      = $rootScope.$new();
     $controller = $injector.get("$controller");
-    loginCtrl = $controller("LoginCtrl", {'$scope':$scope});
-
+    loginCtrl   = $controller("LoginCtrl", {'$scope':$scope});
   }));
 
   it('has a LoginCtrl controller', function() {
-      // var scope = $rootScope.$new();
-      // var ctrl  = $controller('LoginCtrl', {$scope: scope});
       expect(loginCtrl).toBeDefined();
-
   });
 
-  // it('submits a email as a username', function() {
-  //   $scope.user.email === "bob";
-  // });
+  describe('.submit', function() {
+
+    it('is called when the user presses the "submit" button', function() {
+
+    });
+
+  });
 
 });
