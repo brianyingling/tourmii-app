b6ddaf1ee8258b9c79a7599be60fc7b70d53c9e8
Brian Yingling <brian_yingling@yahoo.com>
2014-05-19 13:36:49 -0400

    working on tests to recognize LoginCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,10 @@
 angular.module('tourmii.controllers', [])
 
 .controller('LoginCtrl', function($scope) {
+  debugger;
+  return true;
+  // $scope.user = {};
+  // $scope.user.email = "bob";
 })
 
 .controller('RegisterCtrl', function($scope) {
