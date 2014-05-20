06b4aca6f1821f655a6401b63ec44a6b2c079fda
Brian Yingling <brian_yingling@yahoo.com>
2014-05-20 10:08:45 -0400

    implemented $httpBackend test

--- test/controllers/loginCtrlSpec.js
+++ test/controllers/loginCtrlSpec.js
@@ -1,13 +1,14 @@
 describe('LoginCtrl', function() {
-  var $scope, $rootScope, $controller, createController;
+  var $scope, $rootScope, $controller, $httpBackend;
   var loginCtrl;
 
   beforeEach(module('tourmii.controllers'));
   beforeEach(inject(function($injector){
-    $rootScope  = $injector.get('$rootScope');
-    $scope      = $rootScope.$new();
-    $controller = $injector.get("$controller");
-    loginCtrl   = $controller("LoginCtrl", {'$scope':$scope});
+    $rootScope   = $injector.get('$rootScope');
+    $httpBackend = $injector.get('$httpBackend');
+    $scope       = $rootScope.$new();
+    $controller  = $injector.get("$controller");
+    loginCtrl    = $controller("LoginCtrl", {'$scope':$scope});
   }));
 
   it('has a LoginCtrl controller', function() {
@@ -15,11 +16,27 @@ describe('LoginCtrl', function() {
   });
 
   describe('.submit', function() {
+    it('receives a user obj on successful HTTP request', function() {
+        var resp = {
+          user: {
+            tours: [
+              {description:'tours'},
+              {descrioption:'more tours'}
+            ]
+          }
+        };
 
-    it('is called when the user presses the "submit" button', function() {
-
+        $httpBackend.expectPOST('http://localhost:3000/login')
+          .respond(resp);
+        $scope.submit();
+        $httpBackend.flush();
     });
 
+  });
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
 
 });
