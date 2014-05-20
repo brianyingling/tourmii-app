915102fd2c2cfb65f67d1081328310fd7b84775c
Brian Yingling <brian_yingling@yahoo.com>
2014-05-20 10:43:37 -0400

    removed unnecessary comments

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,26 +2,23 @@ angular.module('tourmii.controllers', [])
 
 .controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.user = {};
+  $scope.errors = [];
 
   $scope.submit = function(){
+    $scope.errors = [];
     $scope.user.email    = $scope.email;
     $scope.user.password = $scope.password;
     $http.post("http://localhost:3000/login", $scope.user)
       .success(function(data) {
-        // data is the user object with all of his/her tours
-        // debugger;
-        localStorage['tourmii_session_id'] = data.user.id;
-
-        $scope.tours = data.user.tours;
         console.log(data);
+        localStorage['tourmii_session_id'] = data.user.id;
+        $scope.tours = data.user.tours;
       })
       .error(function(data) {
-        // debugger;
         console.log(data);
+        $scope.errors.push(data.error.message);
       });
   };
-
-
 }])
 
 .controller('RegisterCtrl', ['$scope', function($scope) {
