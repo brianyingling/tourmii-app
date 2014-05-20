0f6f10f767cec7126f45b3a4d2bf3c52e0d8535d
Brian Yingling <brian_yingling@yahoo.com>
2014-05-20 15:04:15 -0400

    added tests for localStorage and added 401 error code to mock error from server

--- test/controllers/registerCtrlSpec.js
+++ test/controllers/registerCtrlSpec.js
@@ -1,7 +1,8 @@
 describe('RegisterCtrl', function() {
 
   var $rootScope, $httpBackend, $scope, $controller, registerCtrl;
-  var url = 'http://localhost:3000/users';
+  var url   = 'http://localhost:3000/users';
+  var store = {};
   var resp = {
         user: {
           id: 1,
@@ -11,6 +12,7 @@ describe('RegisterCtrl', function() {
         },
       };
   var data = {
+        status: "ERROR!",
         error: {
           messages: {
             email: "has already been taken"
@@ -27,21 +29,36 @@ describe('RegisterCtrl', function() {
     $scope       = $rootScope.$new();
     $controller  = $injector.get('$controller');
     registerCtrl = $controller('RegisterCtrl', {'$scope':$scope});
+
+    spyOn(localStorage, 'setItem').andCallFake(function(key, value) {
+      return store[key] = value;
+    });
+
   }));
 
   describe('submit', function() {
     it('receives a user obj on successful signup', function() {
-      $httpBackend.expectPOST(url)
-        .respond(resp);
+      $httpBackend.expectPOST(url).respond(resp);
       $scope.submit();
       $httpBackend.flush();
     });
 
     it('receives error messages when signup fails', function() {
-      $httpBackend.expectPOST(url).respond(data);
+      $httpBackend.expectPOST(url).respond(401, data);
         $scope.submit();
         $httpBackend.flush();
         expect($scope.errors).toBeDefined();
+    });
+
+    it('assigns the session id as a localStorage parameter', function() {
+      $httpBackend.expectPOST(url).respond(resp);
+      $scope.status = "ERROR";
+      $scope.submit();
+      $httpBackend.flush();
+      console.log(store);
+      console.log(resp.user.id);
+      var id = resp.user.id;
+      expect(store).toBeDefined();
     });
 
     afterEach(function() {
