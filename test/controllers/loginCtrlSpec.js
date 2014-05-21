eb967856a478204381b22d7c5ad2a5d0a55db57d
Brian Yingling <brian_yingling@yahoo.com>
2014-05-21 13:04:32 -0400

    added stateMock module bc ui-router interferes with  when running specs. Doesn't work, so Im checking for every request in spec

--- test/controllers/loginCtrlSpec.js
+++ test/controllers/loginCtrlSpec.js
@@ -2,13 +2,19 @@ describe('LoginCtrl', function() {
   var $scope, $rootScope, $controller, $httpBackend;
   var loginCtrl;
 
+  beforeEach(module('stateMock'));
+  beforeEach(module('tourmii'));
   beforeEach(module('tourmii.controllers'));
-  beforeEach(inject(function($injector){
+
+  beforeEach(inject(function($injector, _$state_){
+    $state       = _$state_;
     $rootScope   = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
     $scope       = $rootScope.$new();
     $controller  = $injector.get("$controller");
     loginCtrl    = $controller("LoginCtrl", {'$scope':$scope});
+
+
   }));
 
   it('has a LoginCtrl controller', function() {
@@ -32,20 +38,27 @@ describe('LoginCtrl', function() {
 
     });
     it('receives a user obj on successful HTTP request', function() {
+        $httpBackend.expectGET('templates/tabs.html').respond(true);
+        $httpBackend.expectGET('templates/tab-login.html').respond(true);
+        $httpBackend.expectGET('templates/tab-register.html').respond(true);
+
         expect($scope.user).toBeDefined();
         $httpBackend.flush();
     });
 
     it('saves the session id to localStorage on successful HTTP request', function() {
+      $httpBackend.expectGET('templates/tabs.html').respond(true);
+      $httpBackend.expectGET('templates/tab-login.html').respond(true);
+      $httpBackend.expectGET('templates/tab-register.html').respond(true);
+
+
       expect(localStorage['tourmii_session_id']).toBeDefined();
       $httpBackend.flush();
     });
 
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
   });
-
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-  });
-
 });
