e462baf9b45130a20ec81910954240ed6e9600df
Brian Yingling <brian_yingling@yahoo.com>
2014-05-23 12:29:39 -0400

    google places service spec needs some work

---
+++ test/googlePlacesServiceSpec.js
@@ -0,0 +1,38 @@
+// describe('googlePlacesService', function() {
+//   var $injector, googlePlacesService, googleTemp;
+//   var google = {
+//     maps : {
+//         OverlayView : function () {
+//         },
+//         Marker : function () {
+//         },
+//         InfoWindow : function () {
+//         }
+//     }
+// };
+
+//   beforeEach(function() {
+//     spyOn(window.document.getElementById).andReturn("<div id='map'></div>");
+//     // if(window.google) googleTemp = google;
+//     // window.google = google;
+//   });
+
+//   beforeEach(module('tourmii'));
+//   beforeEach(module('tourmii.services'));
+
+//   beforeEach(inject(function(_$injector_) {
+//     $injector = _$injector_;
+//     googlePlacesService = $injector.get('googlePlacesService');
+//   }));
+
+//   describe('.getDetails', function() {
+//     it('retrieves details on a place given a reference', function() {
+
+//     });
+//   });
+
+//   afterEach(function() {
+//     window.google = googleTemp;
+//   });
+
+// });
