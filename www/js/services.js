5bcef17e223793af368967483d4db9e23bf11207
Brian Yingling <brian_yingling@yahoo.com>
2014-05-29 17:04:28 -0400

    added GoogleMap factory

--- www/js/services.js
+++ www/js/services.js
@@ -42,15 +42,55 @@ angular.module('tourmii.services', [])
     }
   };
 })
+.factory('googleMap', function(){
+    return {
+        createMap: function(element, options){
+            return new google.maps.Map(element, options);
+        },
+        position: function(location){
+            return new google.maps.LatLng(location.latitude, location.longitude);
+        },
+        geocode: function(search, successFun){
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode(search, successFun);
+        },
+        bounds: function(){
+            return new google.maps.LatLngBounds();
+        },
+        placesService: function(map, lat, lng, zoom) {
+          return new google.maps.places.PlacesService(map, {
+            center: new google.maps.LatLng(lat, lng),
+            zoom:   zoom
+          });
+        },
+        mapOptions: {
+            animation: google.maps.MapTypeId.DROP,
+            maxZoom: 15,
+            mapTypeControlOptions: {
+                position: google.maps.ControlPosition.LEFT_BOTTOM,
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            }
+        },
+        markerOptions:{
+            animation: google.maps.Animation.DROP,
+            clickable: true
+        }
+    };
+})
 
-.service('googlePlacesService', ['$q', '$http', function($q, $http) {
+// .service('googlePlacesService', ['$q', '$http', function($q, $http) {
+//   var GOOGLE_PLACES_API_KEY = "AIzaSyCvzuNHRQq5SRJZnyqPJ6c5nMzyeDm2kU0";
+//   var map     = new google.maps.Map(document.getElementById('map'));
+//   var service = new google.maps.places.PlacesService(map, {
+//     center: new google.maps.LatLng(40.859239040, -74.437774074),
+//     zoom: 15
+//   });
+
+.service('googlePlacesService', ['$q', '$http', 'googleMap', function($q, $http, googleMap) {
   var GOOGLE_PLACES_API_KEY = "AIzaSyCvzuNHRQq5SRJZnyqPJ6c5nMzyeDm2kU0";
-  var map     = new google.maps.Map(document.getElementById('map'));
-  var service = new google.maps.places.PlacesService(map, {
-    center: new google.maps.LatLng(40.859239040, -74.437774074),
-    zoom: 15
-  });
-
+  var map = googleMap.createMap(document.getElementById('map'));
+  var service = googleMap.placesService(map, 40.859239040, 74.437774074, 15);
+  debugger;
   return {
     getPlaceDetails: function(ref) {
       var request  = {reference:ref};
