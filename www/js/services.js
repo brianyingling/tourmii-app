9d6a6b0b1f373c95b799c88301834ffbeab55d2e
Brian Yingling <brian_yingling@yahoo.com>
2014-05-30 13:09:25 -0400

    adding options to map

--- www/js/services.js
+++ www/js/services.js
@@ -44,44 +44,48 @@ angular.module('tourmii.services', [])
 })
 .factory('googleMap', function(){
     return {
-        createMap: function(element, options){
-            return new google.maps.Map(element, options);
-        },
-        position: function(location){
-            return new google.maps.LatLng(location.latitude, location.longitude);
-        },
-        geocode: function(search, successFun){
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode(search, successFun);
-        },
-        bounds: function(){
-            return new google.maps.LatLngBounds();
-        },
-        placesService: function(map, lat, lng, zoom) {
-          return new google.maps.places.PlacesService(map, {
-            center: new google.maps.LatLng(lat, lng),
-            zoom:   zoom
-          });
-        },
-        mapOptions: {
-            animation: google.maps.MapTypeId.DROP,
-            maxZoom: 15,
-            mapTypeControlOptions: {
-                position: google.maps.ControlPosition.LEFT_BOTTOM,
-                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-            }
-        },
-        markerOptions:{
-            animation: google.maps.Animation.DROP,
-            clickable: true
-        }
+      createMap: function(element, options){
+        return new google.maps.Map(element, options);
+      },
+      position: function(location){
+        return new google.maps.LatLng(location.latitude, location.longitude);
+      },
+      geocode: function(search, successFun){
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode(search, successFun);
+      },
+      bounds: function(){
+        return new google.maps.LatLngBounds();
+      },
+      placesService: function(map, lat, lng, zoom) {
+        return new google.maps.places.PlacesService(map, {
+          center: new google.maps.LatLng(lat, lng),
+          zoom:   zoom
+        });
+      },
+      mapOptions: {
+          animation: google.maps.MapTypeId.DROP,
+          maxZoom: 15,
+          mapTypeControlOptions: {
+              position: google.maps.ControlPosition.LEFT_BOTTOM,
+              style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+          }
+      },
+      markerOptions:{
+          animation: google.maps.Animation.DROP,
+          clickable: true
+      }
     };
 })
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AIzaSyCvzuNHRQq5SRJZnyqPJ6c5nMzyeDm2kU0
 .service('googlePlacesService', ['$q', '$http', 'googleMap', function($q, $http, googleMap) {
   var GOOGLE_PLACES_API_KEY = "AIzaSyCvzuNHRQq5SRJZnyqPJ6c5nMzyeDm2kU0";
-  var map = googleMap.createMap(document.getElementById('map'));
-  var service = googleMap.placesService(map, 40.859239040, 74.437774074, 15);
+  var map      = googleMap.createMap(document.getElementById('map'), googleMap.mapOptions);
+  var service  = googleMap.placesService(map, 40.859239040, -74.437774074, 15);
+  debugger;
+  var sanitize = function(query) {
+    return query.replace(' ', '+');
+  };
 
   return {
     getPlaceDetails: function(ref) {
@@ -101,7 +105,7 @@ angular.module('tourmii.services', [])
         // hardcoded -- dynamic coords will be added later.
         location: new google.maps.LatLng(40.859239040, -74.437774074),
         radius:   '500',
-        query:    query
+        query:    sanitize(query)
       };
 
       service.textSearch(req, function(results, status) {
